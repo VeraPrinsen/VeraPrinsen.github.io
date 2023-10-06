72165908b7d1ea8edf451e44f80078471f3f8418
Vera Prinsen <vera.prinsen@nedap.com>
2023-10-06 19:00:04 +0200

    Make page only one button for now

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -12,14 +12,21 @@ const App = () => {
         "Custom yes/no door label"
     ]
 
+    const openInNewTab = (url) => {
+        window.open(url, "_blank", "noreferrer");
+    };
+
     return (
-        <div className="app">
-            <header className="main-frame">
-                {currentPage === "menu" && <Menu pages={pages} setPage={setPage} />}
-                {currentPage === pages[0] && <Page title={pages[0]} setPage={setPage} content={<Boardgames />} />}
-                {currentPage === pages[1] && <Page title={pages[1]} setPage={setPage} content={<div>Content</div>} />}
-            </header>
+        <div>
+            <button onClick={() => openInNewTab("https://betaalverzoek.rabobank.nl/betaalverzoek/?id=nhbnMrYkQMKje6uc1xwHaQ")}>Betaal gripje(s)</button>
         </div>
+        // <div className="app">
+        //     <header className="main-frame">
+        //         {currentPage === "menu" && <Menu pages={pages} setPage={setPage} />}
+        //         {currentPage === pages[0] && <Page title={pages[0]} setPage={setPage} content={<Boardgames />} />}
+        //         {currentPage === pages[1] && <Page title={pages[1]} setPage={setPage} content={<div>Content</div>} />}
+        //     </header>
+        // </div>
     );
 }
 

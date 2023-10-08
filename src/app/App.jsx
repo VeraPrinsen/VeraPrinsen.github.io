98ec1556a06bd96f1a3179b2bb25c5441f579ac3
Vera Prinsen <vera.prinsen@nedap.com>
2023-10-08 17:15:00 +0200

    test routes

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,28 +1,14 @@
 import './stylesheets/App.css';
-import { Route, Routes, Link } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import PaymentRequest from "./PaymentRequest";
 import Homepage from "./Homepage";
 
 const App = () => {
     return (
-        <div className="App">
-            <div>
-                <nav>
-                    <ul id="navigation">
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/gripjes">Gripjes</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-            <Routes>
-                <Route exact path="/" element={<Homepage />} />
-                <Route path="/gripjes" element={<PaymentRequest />} />
-            </Routes>
-        </div>
+        <Routes>
+            <Route exact path="/" element={<Homepage />} />
+            <Route path="/gripjes" element={<PaymentRequest />} />
+        </Routes>
     )
 }
 

23964108b3f3b2228f81875a32ad3ab3892d9604
Vera Prinsen <vera.prinsen@nedap.com>
2023-10-08 17:11:51 +0200

    Testing router

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,15 +1,29 @@
 import './stylesheets/App.css';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 import PaymentRequest from "./PaymentRequest";
 import Homepage from "./Homepage";
 
-const router = createBrowserRouter([
-    { path: "", element: <Homepage /> },
-    { path: "/gripjes", element: <PaymentRequest/> }
-])
-
 const App = () => {
-    return <RouterProvider router={router} />
+    return (
+        <div className="App">
+            <div>
+                <nav>
+                    <ul id="navigation">
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/gripjes">Gripjes</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <Routes>
+                <Route exact path="/" element={<Homepage />} />
+                <Route path="/gripjes" element={<PaymentRequest />} />
+            </Routes>
+        </div>
+    )
 }
 
 export default App;

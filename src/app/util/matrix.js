57a16298b536dc1ebeadc1dca01caa066f8ea243
Vera Prinsen <vera.prinsen@nedap.com>
2023-10-14 23:14:30 +0200

    Technical test

---
+++ src/app/util/matrix.js
@@ -0,0 +1,61 @@
+const matrix = (nRows, nCols) => {
+    let rowStart = 2
+    let rowEnd = nRows
+    let colStart = 1
+    let colEnd = nCols
+
+    let currentIndex = 1
+    let direction = "E" // N, E, S, W
+    let currentRow = 1
+    let currentCol = 1
+
+    let totalNumbers = nRows * nCols
+    let currentNumbers = 0
+    const output = []
+    while (currentNumbers < totalNumbers) {
+        output.push(currentIndex)
+
+        switch (direction) {
+            case "E":
+                currentIndex = currentIndex + 1
+                currentCol = currentCol + 1
+
+                if (currentCol >= colEnd) {
+                    direction = "S"
+                    colEnd = colEnd - 1
+                }
+                break;
+            case "S":
+                currentIndex = currentIndex + nCols
+                currentRow = currentRow + 1
+
+                if (currentRow >= rowEnd) {
+                    direction = "W"
+                    rowEnd = rowEnd - 1
+                }
+                break;
+            case "W":
+                currentIndex = currentIndex - 1
+                currentCol = currentCol - 1
+
+                if (currentCol <= colStart) {
+                    direction = "N"
+                    colStart = colStart + 1
+                }
+                break;
+            case "N":
+                currentIndex = currentIndex - nCols
+                currentRow = currentRow - 1
+
+                if (currentRow <= rowStart) {
+                    direction = "E"
+                    rowStart = rowStart + 1
+                }
+                break;
+        }
+        currentNumbers = currentNumbers + 1
+    }
+    console.log(output.join(" "))
+}
+
+export default matrix
---
+++ src/app/util/snakesAndLadders.js
@@ -0,0 +1,50 @@
+const SNAKES_OR_LADDERS = new Map([
+    [1,1],
+    [2,2],
+    [3,11],
+    [4,4],
+    [5,5],
+    [6,17],
+    [7,7],
+    [8,8],
+    [9,18],
+    [10,12],
+    [11,11],
+    [12,12],
+    [13,13],
+    [14,4],
+    [15,15],
+    [16,16],
+    [17,17],
+    [18,18],
+    [19,8],
+    [20,20],
+    [21,21],
+    [22,20],
+    [23,23],
+    [24,16],
+    [25,25]
+])
+
+const play = () => {
+    let playerLocation = 1
+    while (playerLocation < 25) {
+        console.log("Player location: " + playerLocation)
+        let diceRoll = Math.floor(Math.random() * 5) + 1
+        console.log("Diceroll: " + diceRoll)
+        playerLocation = playerLocation + diceRoll
+        let snakeOrLadderLocation =  SNAKES_OR_LADDERS.get(playerLocation)
+        if (playerLocation !== snakeOrLadderLocation) {
+            console.log("Player location: " + playerLocation)
+            if (snakeOrLadderLocation > playerLocation) {
+                console.log("LADDER :)")
+            } else {
+                console.log("SNAKE :(")
+            }
+            playerLocation = snakeOrLadderLocation
+        }
+    }
+    console.log("YOU WON!")
+}
+
+export default play

9ac4d2d2c8cfe6452977395ea5bd0c5f77c51598
Vera Prinsen <vera.prinsen@nedap.com>
2023-11-06 23:45:42 +0100

    Add notes button

--- src/app/sudoku/components/Sudoku.jsx
+++ src/app/sudoku/components/Sudoku.jsx
@@ -3,18 +3,35 @@ import {useContext} from "react";
 import { SudokuContext } from "../store/SudokuContext";
 
 import "../stylesheets/Sudoku.scss"
+import {NOTES_OFF, NOTES_ON} from "../util/constants";
 
 const Sudoku = () => {
-    const { onCellClick } = useContext(SudokuContext)
+    const { mode, onCellClick } = useContext(SudokuContext)
 
-    const handleClick = e => {
+    const handleScreenClick = e => {
         e.stopPropagation()
         onCellClick(null, null)
     }
 
+    const handleButtonClick = () => {
+
+    }
+
+    let modeText
+    if (mode === NOTES_ON) {
+        modeText = "on"
+    } else if (mode === NOTES_OFF) {
+        modeText = "off"
+    }
+
     return (
-        <div className="sudoku-main" onClick={handleClick}>
+        <div className="sudoku-main" onClick={handleScreenClick}>
             <SudokuGrid />
+            <div className="actions">
+                <button onClick={handleButtonClick}>Notes</button>
+                {modeText}
+            </div>
+
         </div>
     )
 }

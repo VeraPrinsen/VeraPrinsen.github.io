bcedfa0151184887362330695ba5650595ab0a58
Vera Prinsen <33416829+VeraPrinsen@users.noreply.github.com>
2025-03-19 11:43:52 +0100

    add search term input (#20)

--- src/app/botc/components/characters/characterSelector/CharacterSelector.jsx
+++ src/app/botc/components/characters/characterSelector/CharacterSelector.jsx
@@ -1,20 +1,31 @@
 import "./CharacterSelector.scss";
 import CharacterList from "./characterList/CharacterList";
+import { useState } from "react";
 
 const CharacterSelector = ({
-                               characters,
-                               selectedCharacters,
-                               onCharacterAdd,
-                               onCharacterDelete
-}) => {
-    return (
-        <div className="character-selector">
-            <div className="filter">FILTER</div>
-            <div className="search-bar">SEARCH</div>
-            <CharacterList characters={characters} selectedCharacters={selectedCharacters} onCharacterAdd={onCharacterAdd} onCharacterDelete={onCharacterDelete} />
-            <div className="actions">CLEAR</div>
-        </div>
-    )
+	                           characters,
+	                           selectedCharacters,
+	                           onCharacterAdd,
+	                           onCharacterDelete
+                           }) => {
+	const [searchTerm, setSearchTerm] = useState("")
+
+	const handleSearchTermChange = (e) => {
+		e.preventDefault()
+		setSearchTerm(e.target.value)
+	}
+
+	const searchedCharacters = characters.filter(c => c.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
+	return (
+		<div className="character-selector">
+			<div className="filter">FILTER</div>
+			<div className="search-bar"><input className='search-input' onChange={handleSearchTermChange}/></div>
+			<CharacterList characters={searchedCharacters} selectedCharacters={selectedCharacters}
+			               onCharacterAdd={onCharacterAdd} onCharacterDelete={onCharacterDelete}/>
+			<div className="actions">CLEAR</div>
+		</div>
+	)
 }
 
 export default CharacterSelector

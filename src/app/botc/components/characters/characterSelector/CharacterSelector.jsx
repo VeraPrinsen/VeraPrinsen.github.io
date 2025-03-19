a70c8708a35f5e942c1577d489e0438174dffcbe
Vera Prinsen <vera.prinsen@nedap.com>
2025-03-19 14:48:33 +0100

    Add filtering on edition

--- src/app/botc/components/characters/characterSelector/CharacterSelector.jsx
+++ src/app/botc/components/characters/characterSelector/CharacterSelector.jsx
@@ -9,19 +9,56 @@ const CharacterSelector = ({
 	                           onCharacterDelete
                            }) => {
 	const [searchTerm, setSearchTerm] = useState("")
+	const [filterOptions, toggleFilterOptions] = useState({
+		"Trouble Brewing": true,
+		"Bad Moon Rising": true,
+		"Sects & Violets": true,
+		"Kickstarter Experimental": true,
+		"Unreleased Experimental": true,
+		"Townsfolk": true,
+		"Outsider": true,
+		"Minion": true,
+		"Demon": true,
+		"Fabled": true
+	})
 
 	const handleSearchTermChange = (e) => {
 		e.preventDefault()
 		setSearchTerm(e.target.value)
 	}
 
-	const searchedCharacters = characters.filter(c => c.name.toLowerCase().includes(searchTerm.toLowerCase()))
+	const handleToggleFilterOptions = (key) => {
+		toggleFilterOptions(prevState => {
+			return {
+				...prevState,
+				[key]: !prevState[key]
+			}
+		})
+	}
+
+	const filteredCharacters = characters.filter(c => c.name.toLowerCase().includes(searchTerm.toLowerCase()) && filterOptions[c.edition] && filterOptions[c.type])
 
 	return (
 		<div className="character-selector">
-			<div className="filter">FILTER</div>
+			<div className="filter">
+				<div><input type="checkbox" checked={filterOptions["Trouble Brewing"]}
+				            onChange={() => handleToggleFilterOptions("Trouble Brewing")}/> Trouble Brewing
+				</div>
+				<div><input type="checkbox" checked={filterOptions["Bad Moon Rising"]}
+				            onChange={() => handleToggleFilterOptions("Bad Moon Rising")}/> Bad Moon Rising
+				</div>
+				<div><input type="checkbox" checked={filterOptions["Sects & Violets"]}
+				            onChange={() => handleToggleFilterOptions("Sects & Violets")}/> Sects and Violets
+				</div>
+				<div><input type="checkbox" checked={filterOptions["Kickstarter Experimental"]}
+				            onChange={() => handleToggleFilterOptions("Kickstarter Experimental")}/> Kickstarter Experimental
+				</div>
+				<div><input type="checkbox" checked={filterOptions["Unreleased Experimental"]}
+				            onChange={() => handleToggleFilterOptions("Unreleased Experimental")}/> Unreleased Experimental
+				</div>
+			</div>
 			<div className="search-bar"><input className='search-input' onChange={handleSearchTermChange}/></div>
-			<CharacterList characters={searchedCharacters} selectedCharacters={selectedCharacters}
+			<CharacterList characters={filteredCharacters} selectedCharacters={selectedCharacters}
 			               onCharacterAdd={onCharacterAdd} onCharacterDelete={onCharacterDelete}/>
 			<div className="actions">CLEAR</div>
 		</div>

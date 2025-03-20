6d1543d5620cb5893aabd7be48cdc37f4fb1d64a
Vera Prinsen <vera.prinsen@nedap.com>
2025-03-20 16:09:34 +0100

    Edit styling

--- src/app/botc/components/characters/characterSelector/CharacterSelector.jsx
+++ src/app/botc/components/characters/characterSelector/CharacterSelector.jsx
@@ -41,44 +41,48 @@ const CharacterSelector = ({
 	return (
 		<div className="character-selector">
 			<div className="filter">
-				<h4>Script</h4>
-				<div><input type="checkbox" checked={filterOptions["Trouble Brewing"]}
-				            onChange={() => handleToggleFilterOptions("Trouble Brewing")}/> Trouble Brewing
-				</div>
-				<div><input type="checkbox" checked={filterOptions["Bad Moon Rising"]}
-				            onChange={() => handleToggleFilterOptions("Bad Moon Rising")}/> Bad Moon Rising
-				</div>
-				<div><input type="checkbox" checked={filterOptions["Sects & Violets"]}
-				            onChange={() => handleToggleFilterOptions("Sects & Violets")}/> Sects and Violets
-				</div>
-				<div><input type="checkbox" checked={filterOptions["Kickstarter Experimental"]}
-				            onChange={() => handleToggleFilterOptions("Kickstarter Experimental")}/> Kickstarter Experimental
-				</div>
-				<div><input type="checkbox" checked={filterOptions["Unreleased Experimental"]}
-				            onChange={() => handleToggleFilterOptions("Unreleased Experimental")}/> Unreleased Experimental
+				<div className="edition">
+					<h4>Script</h4>
+					<div><input type="checkbox" checked={filterOptions["Trouble Brewing"]}
+					            onChange={() => handleToggleFilterOptions("Trouble Brewing")}/> Trouble Brewing
+					</div>
+					<div><input type="checkbox" checked={filterOptions["Bad Moon Rising"]}
+					            onChange={() => handleToggleFilterOptions("Bad Moon Rising")}/> Bad Moon Rising
+					</div>
+					<div><input type="checkbox" checked={filterOptions["Sects & Violets"]}
+					            onChange={() => handleToggleFilterOptions("Sects & Violets")}/> Sects and Violets
+					</div>
+					<div><input type="checkbox" checked={filterOptions["Kickstarter Experimental"]}
+					            onChange={() => handleToggleFilterOptions("Kickstarter Experimental")}/> Kickstarter Experimental
+					</div>
+					<div><input type="checkbox" checked={filterOptions["Unreleased Experimental"]}
+					            onChange={() => handleToggleFilterOptions("Unreleased Experimental")}/> Unreleased Experimental
+					</div>
 				</div>
 
-				<h4>Type</h4>
-				<div><input type="checkbox" checked={filterOptions["Townsfolk"]}
-				            onChange={() => handleToggleFilterOptions("Townsfolk")}/> Townsfolk
-				</div>
-				<div><input type="checkbox" checked={filterOptions["Outsider"]}
-				            onChange={() => handleToggleFilterOptions("Outsider")}/> Outsider
-				</div>
-				<div><input type="checkbox" checked={filterOptions["Minion"]}
-				            onChange={() => handleToggleFilterOptions("Minion")}/> Minion
-				</div>
-				<div><input type="checkbox" checked={filterOptions["Demon"]}
-				            onChange={() => handleToggleFilterOptions("Demon")}/> Demon
-				</div>
-				<div><input type="checkbox" checked={filterOptions["Fabled"]}
-				            onChange={() => handleToggleFilterOptions("Fabled")}/> Fabled
+				<div className="type">
+					<h4>Type</h4>
+					<div><input type="checkbox" checked={filterOptions["Townsfolk"]}
+					            onChange={() => handleToggleFilterOptions("Townsfolk")}/> Townsfolk
+					</div>
+					<div><input type="checkbox" checked={filterOptions["Outsider"]}
+					            onChange={() => handleToggleFilterOptions("Outsider")}/> Outsider
+					</div>
+					<div><input type="checkbox" checked={filterOptions["Minion"]}
+					            onChange={() => handleToggleFilterOptions("Minion")}/> Minion
+					</div>
+					<div><input type="checkbox" checked={filterOptions["Demon"]}
+					            onChange={() => handleToggleFilterOptions("Demon")}/> Demon
+					</div>
+					<div><input type="checkbox" checked={filterOptions["Fabled"]}
+					            onChange={() => handleToggleFilterOptions("Fabled")}/> Fabled
+					</div>
 				</div>
 			</div>
 			<div className="search-bar"><input className='search-input' onChange={handleSearchTermChange}/></div>
 			<CharacterList characters={filteredCharacters} selectedCharacters={selectedCharacters}
 			               onCharacterAdd={onCharacterAdd} onCharacterDelete={onCharacterDelete}/>
-			<div className="actions">CLEAR</div>
+			<div className="actions"></div>
 		</div>
 	)
 }

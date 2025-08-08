e80a9b48200ee23dfdbdee33a5b0705e48b02ed7
Vera Prinsen <vera.prinsen@nedap.com>
2025-08-08 15:17:04 +0200

    Link to NerdyToolbox

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './app/boardgames/stylesheets/index.css'
-import {HashRouter} from 'react-router-dom'
-import App from './app/App'
+import { Navigate } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-      <HashRouter>
-          <App />
-      </HashRouter>
-  </React.StrictMode>
+  <div>
+    Website has moved to <a href="https://nerdytoolbox.github.io/">NerdyToolbox.github.io</a>
+  </div>
 )
 
 // If you want to start measuring performance in your app, pass a function
